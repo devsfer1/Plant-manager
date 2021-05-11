3ef6f8f2a44c98a28e7ddcfcdce44dbe8588b436
FernandoChaves <fernando.chaves16@hotmail.com>
2021-05-11 18:44:41 -0300

    remove btn component and restyled image height

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,12 +5,12 @@ import {
   Text, 
   Image, 
   TouchableOpacity,
-  StyleSheet
+  StyleSheet,
+  Dimensions
 } from 'react-native';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-import {Button} from '../components/Button';
 
 export function Welcome(){
   return(
@@ -21,14 +21,25 @@ export function Welcome(){
         de forma fácil
       </Text>
 
-      <Image source={wateringImg} style={styles.image} />
+      <Image 
+          source={wateringImg} style={styles.image} 
+          resizeMode="contain"
+      />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar
         você sempre que precisar.
       </Text>
 
-      <Button title=">" />
+      <TouchableOpacity 
+      style={styles.button}
+      activeOpacity={0.8}
+      >
+        <Text style={styles.buttonText}>
+          >
+        </Text>
+      </TouchableOpacity>
+
     </SafeAreaView>
   ) 
 }
@@ -37,7 +48,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   title: {
     fontSize: 32,
@@ -52,6 +63,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     color: colors.heading
   },
+  image: {
+    height: Dimensions.get('window').width * 0.7,
+  },
   button: {
     backgroundColor: colors.green,
     justifyContent: 'center',
@@ -60,10 +74,6 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     height: 56,
     width: 56
-  },
-  image: {
-    width: 292,
-    height: 284,
   },
   buttonText: {
     color: colors.white,
